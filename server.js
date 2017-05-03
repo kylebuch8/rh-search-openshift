95e2617b7079d6e3eb50351d03d2c660efa12f9c
Kyle Buchanan <kylebuch8@gmail.com>
2017-05-03 11:45:36 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const Path = require('path');
+const Hapi = require('hapi');
+const Inert = require('inert');
+const Good = require('good');
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'public')
+      }
+    }
+  }
+});
+
+server.connection({
+  host: process.env.OPENSHIFT_NODEJS_IP || 'localhost',
+  port: process.env.OPENSHIFT_NODEJS_PORT || 8000
+});
+
+server.register([
+  {
+    register: Inert
+  },
+  {
+    register: Good,
+    options: {
+      reporters: {
+        console: [{
+          module: 'good-squeeze',
+          name: 'Squeeze',
+          args: [{
+            response: '*',
+            log: '*'
+          }]
+        }, {
+          module: 'good-console'
+        }, 'stdout']
+      }
+    }
+  }
+], err => {
+  if (err) {
+    throw err;
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/{params*}',
+    handler: {
+      directory: {
+        path: '.',
+        redirectToSlash: true,
+        index: true
+      }
+    }
+  });
+
+  server.ext('onPostHandler', (request, reply) => {
+    const response = request.response;
+    if (response.isBoom && response.output.statusCode === 404) {
+      return reply.file('index.html');
+    }
+
+    return reply.continue();
+  });
+
+  server.start(err => {
+    if (err) {
+      throw err;
+    }
+
+    server.log('info', `Server running at: ${server.info.uri}`);
+  });
+});
